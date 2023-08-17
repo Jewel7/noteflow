977c476b78ac1bbd9dbf778fe3753092a785a0db
Jewel Chiong <jewel@Jewels-MBP.fios-router.home>
2023-08-16 22:23:29 -0400

    [InMemoryCache] chore: add dependency to enable caching

--- src/main/java/com/noteflow/noteflowapp/NoteflowApp.java
+++ src/main/java/com/noteflow/noteflowapp/NoteflowApp.java
@@ -2,12 +2,14 @@ package com.noteflow.noteflowapp;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cache.annotation.EnableCaching;
 
+@EnableCaching
 @SpringBootApplication
 public class NoteflowApp {
 
-	public static void main(String[] args) {
-		SpringApplication.run(NoteflowApp.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(NoteflowApp.class, args);
+    }
 
 }
